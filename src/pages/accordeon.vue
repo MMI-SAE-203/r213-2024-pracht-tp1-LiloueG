245760c66797b1a262e88e32c6e2c2f6355a4729
LiloueG <liloue.guyot25@gmail.com>
2024-04-12 19:23:40 +0200

    feat : pointer down + v-show à toutes les sections

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -1,14 +1,26 @@
+<script setup lang="ts" >
+    import { ref } from 'vue'
+
+    const sectionOpen = ref(1)
+
+</script>
+
+
 <template>
+
     <section>
-        <button class="text-xl">bouton 1</button>
-        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorem aperiam nisi! Atque, reiciendis dolorem earum eos, exercitationem quidem nobis quo suscipit optio sapiente vel nihil doloremque recusandae dignissimos! Assumenda.</p>
+        <button class="text-xl" @pointerdown="sectionOpen = 1/* change `sectionOpen` à 1 */">bouton 1</button>
+        <p v-show="sectionOpen == 1/* teste si `sectionOpen` est égale à 1 */">Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui dolorem aperiam nisi! Atque, reiciendis dolorem earum eos, exercitationem quidem nobis quo suscipit optio sapiente vel nihil doloremque recusandae dignissimos! Assumenda.</p>
     </section>
+
     <section>
-        <button class="text-xl">bouton 2</button>
-        <p>Obcaecati placeat vel dolore? Atque iusto asperiores adipisci, odit vitae iste, magni ipsa provident fugiat eius numquam error laborum consequatur accusamus placeat. Aliquam, cumque. Corporis magni repudiandae obcaecati vero laudantium.</p>
+        <button class="text-xl" @pointerdown="sectionOpen = 2/* change `sectionOpen` à 2 */">bouton 2</button>
+        <p v-show="sectionOpen == 2/* teste si `sectionOpen` est égale à 2 */">Obcaecati placeat vel dolore? Atque iusto asperiores adipisci, odit vitae iste, magni ipsa provident fugiat eius numquam error laborum consequatur accusamus placeat. Aliquam, cumque. Corporis magni repudiandae obcaecati vero laudantium.</p>
     </section>
+
     <section>
-        <button class="text-xl">bouton 3</button>
-        <p>Tempore quis, numquam at ipsum distinctio reprehenderit consectetur inventore unde perferendis dolorem dignissimos ut veniam voluptatibus officiis eveniet sapiente cupiditate nesciunt pariatur quasi quia? Vitae maxime sint hic voluptatem et.</p>
+        <button class="text-xl" @pointerdown="sectionOpen = 3/* change `sectionOpen` à 3 */">bouton 3</button>
+        <p v-show="sectionOpen == 3/* teste si `sectionOpen` est égale à 3 */">Tempore quis, numquam at ipsum distinctio reprehenderit consectetur inventore unde perferendis dolorem dignissimos ut veniam voluptatibus officiis eveniet sapiente cupiditate nesciunt pariatur quasi quia? Vitae maxime sint hic voluptatem et.</p>
     </section>
+
 </template>
